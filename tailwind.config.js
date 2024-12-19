d63e79a0cfcc4dcb2bec520af538de9c8ff46cd1
Anton Victor <aantonvictor25@gmail.com>
2024-12-19 17:50:24 +0200

    match design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ['./index.html'],
   theme: {
     extend: {
       colors: {
-        skyblue: "#73cde5",
+        skyblue: '#73cde5',
+        yaleblue: '#174790',
       },
       fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-        urbanist: ["Urbanist", "sans-serif"],
+        poppins: ['Poppins', 'sans-serif'],
+        urbanist: ['Urbanist', 'sans-serif'],
+      },
+      borderWidth: {
+        heroborder: 'var(--hero-border-width)',
+      },
+      spacing: {
+        heroborder: 'var(--hero-border-width)',
       },
     },
   },
